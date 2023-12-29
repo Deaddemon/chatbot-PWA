afe1012590183e5a3e60b34ccacb6f90f0580260
vinita poonia <vinitapoonia333@gmail.com>
2023-12-29 20:24:38 +0530

    Initial React Repo setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import Chatbot from './components/chatbot/Chatbot';
+import AudioRecorder from './components/chatbot/Chatbot';
+const App = () => {
+  return (
+    <div >
+      <Chatbot/>
+    </div>
+  );
+};
+
+
+
+
+export default App;
